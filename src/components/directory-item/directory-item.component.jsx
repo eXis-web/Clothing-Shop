7504a3d369d60c9f7d94274cf11a0e528556e274
Ksenia <varosianarmen92@gmail.com>
2024-01-07 20:34:15 +0100

    made redirecting from shop with useNavigate hook

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,10 +1,13 @@
-import {BackgroundImage,Body,DirectoryItemContainer } from './directory-item.styles.jsx';
+import { useNavigate } from 'react-router';
+import { BackgroundImage, Body, DirectoryItemContainer } from './directory-item.styles.jsx';
 
 const DirectoryItem = ({ category }) => {
-    const { imageUrl, title } = category;
+    const { imageUrl, title, route } = category;
+    const navigate = useNavigate();
 
+    const onNavigateHandler = () => navigate(route);
     return (
-        <DirectoryItemContainer>
+        <DirectoryItemContainer onClick={onNavigateHandler}>
             <BackgroundImage
                 imageUrl={imageUrl}
             />

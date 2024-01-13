459a64c048718f7b314cca4a7bf14b77d565eedd
Ksenia <varosianarmen92@gmail.com>
2024-01-13 20:48:08 +0100

    added store yo index,js , created root-reducer - user.reduser , stor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+
 import './index.scss';
 import {App} from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,19 +9,21 @@ import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './context/user.context';
 import { CategoriesProvider } from './context/categories.context';
 import { CartProvider } from './context/cart.context';
-
+import { store } from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Clothing-Shop">
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter basename="/Clothing-Shop">
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+        </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 

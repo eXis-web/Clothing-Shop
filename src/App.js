413237c3220199b4ce536b2045111df2f915ff28
Ksenia <varosianarmen92@gmail.com>
2024-02-19 22:15:59 +0100

    commented few things

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import {
-  onAuthStateChangedListener,
-  createUserDocumentFromAuth,
-} from './utils/firebase/firebase.utils';
+// import {
+//   onAuthStateChangedListener,
+//   createUserDocumentFromAuth,
+// } from './utils/firebase/firebase.utils';
 
 import Home from './routes/home/home.components'
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
-import { setCurrentUser } from 'store/user/user.action';
+// import { setCurrentUser } from 'store/user/user.action';
 import { getCurrentUser } from './utils/firebase/firebase.utils';
 export const App = () => {
   const dispatch = useDispatch();

57046cc09cd1a74e36a3b065235146bbe6f350d8
Ksenia <varosianarmen92@gmail.com>
2024-03-03 21:45:07 +0100

    invited Stripe to app , index, created stripe.utils, .env(moved to gitignore) files

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import './index.scss';
+import { Elements } from '@stripe/react-stripe-js';
+
 import {App} from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { store, persistor}  from './store/store';
+import { stripePromise } from 'utils/stripe/stripe.utils';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
             <BrowserRouter basename="/Clothing-Shop">
-                <App />
+                <Elements stripe={stripePromise}>
+                    <App />
+                </Elements>    
             </BrowserRouter>
         </PersistGate>
     </Provider>

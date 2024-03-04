aab153ee576c18a99fd787d0f4ee348675e5370b
Ksenia <varosianarmen92@gmail.com>
2024-03-04 17:01:36 +0100

    commented warnings

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,5 +1,5 @@
-import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
-
+import { CardElement } from "@stripe/react-stripe-js";
+// , useStripe, useElements
 import Button, {BUTTON_TYPE_CLASSES} from "../button/button.component";
 import { FormContainer,PaymentFormContainer} from './payment-form.styles.jsx';
 
